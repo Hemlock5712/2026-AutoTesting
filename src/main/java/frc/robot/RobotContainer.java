047b8483c8189ed98131e2167a64fa6252885ca2
Gray Matter <graymatter@hemlockps.com>
2026-03-22 19:56:24 -0400

    Alliance zone speed logic fixes

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -207,22 +207,24 @@ public class RobotContainer {
     // Automatically shoot when able, unless right trigger is pressed
     new Trigger(() -> superstructure.shouldShoot() && DriverStation.isTeleopEnabled())
         .and(joystick.rightTrigger().negate())
-        .whileTrue(
-            Commands.parallel(
-                superstructure.autoShoot(),
-                new TurretDrive(
-                    drivetrain,
-                    () -> {
-                      // Not the cleanest but calculate scaled joystick values
-                      Vector<N2> scaled =
-                          rescaleTranslation(joystick.getLeftY(), joystick.getLeftX());
-                      translationVel[0] = -scaled.get(0) * maxShootSpeed;
-                      translationVel[1] = -scaled.get(1) * maxShootSpeed;
-                      return translationVel[0];
-                    },
-                    () -> translationVel[1],
-                    () -> -rescaleInputs(joystick.getRightX()) * maxShootAngularRate)))
+        .whileTrue(superstructure.autoShoot())
         .onFalse(superstructure.stopShoot());
+
+    new Trigger(() -> superstructure.isInAllianceZone() && DriverStation.isTeleopEnabled())
+        .and(joystick.leftBumper().negate())
+        .and(joystick.rightBumper().negate())
+        .whileTrue(
+            new TurretDrive(
+                drivetrain,
+                () -> {
+                  // Not the cleanest but calculate scaled joystick values
+                  Vector<N2> scaled = rescaleTranslation(joystick.getLeftY(), joystick.getLeftX());
+                  translationVel[0] = -scaled.get(0) * maxShootSpeed;
+                  translationVel[1] = -scaled.get(1) * maxShootSpeed;
+                  return translationVel[0];
+                },
+                () -> translationVel[1],
+                () -> -rescaleInputs(joystick.getRightX()) * maxShootAngularRate));
   }
 
   public Command getAutonomousCommand() {
