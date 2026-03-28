5b477c39dde1d2372630df3ef5a5467224e0d490
Gray Matter <graymatter@hemlockps.com>
2026-03-27 22:00:53 -0400

    money

--- src/main/java/frc/robot/subsystems/shooter/ShooterLookup.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterLookup.java
@@ -19,18 +19,33 @@ public class ShooterLookup {
     buildFeedTime();
   }
 
+  // Home
+  // private static void buildFlywheel() {
+  //   flywheelMap.put(0.0, 20.0);
+  //   flywheelMap.put(1.0, 20.0);
+  //   flywheelMap.put(1.5, 22.0);
+  //   flywheelMap.put(2.0, 26.0);
+  //   flywheelMap.put(2.5, 27.0);
+  //   flywheelMap.put(3.0, 29.0);
+  //   flywheelMap.put(3.5, 32.0);
+  //   flywheelMap.put(4.0, 35.0);
+  //   flywheelMap.put(4.5, 38.0);
+  //   flywheelMap.put(5.0, 39.0);
+  //   flywheelMap.put(5.5, 43.0);
+  // }
+
   private static void buildFlywheel() {
-    flywheelMap.put(0.0, 20.0);
-    flywheelMap.put(1.0, 20.0);
-    flywheelMap.put(1.5, 22.0);
-    flywheelMap.put(2.0, 26.0);
-    flywheelMap.put(2.5, 27.0);
-    flywheelMap.put(3.0, 29.0);
-    flywheelMap.put(3.5, 32.0);
-    flywheelMap.put(4.0, 35.0);
-    flywheelMap.put(4.5, 38.0);
-    flywheelMap.put(5.0, 39.0);
-    flywheelMap.put(5.5, 43.0);
+    flywheelMap.put(0.0, 22.0);
+    flywheelMap.put(1.0, 22.0);
+    flywheelMap.put(1.5, 24.0);
+    flywheelMap.put(2.0, 28.0);
+    flywheelMap.put(2.5, 29.0);
+    flywheelMap.put(3.0, 31.0);
+    flywheelMap.put(3.5, 34.0);
+    flywheelMap.put(4.0, 37.0);
+    flywheelMap.put(4.5, 40.0);
+    flywheelMap.put(5.0, 41.0);
+    flywheelMap.put(5.5, 45.0);
   }
 
   private static void buildHood() {
